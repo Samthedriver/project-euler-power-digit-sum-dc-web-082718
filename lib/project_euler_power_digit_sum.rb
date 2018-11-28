b385f34e1cc0f8a56c134b903e1c1854c0f47237
github username <github email address>
2018-11-28 09:56:10 -0500

    Completed project euler power digit sum.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,25 @@
 # code your solution here
+def power(x, n)
+  if n == 0
+    return 1
+  else
+    return (x * power(x, n-1))
+  end
+end
+
+def digit_sum(x)
+  x_string = x.to_s
+  range = x_string.length - 1
+  sum = 0
+
+  for i in 0..range
+    sum = sum + x_string[i].to_i
+  end
+  return sum
+end
+
+def power_digit_sum(x, n)
+  digit_sum(power(x,n))
+end
+
+puts power_digit_sum(2, 4)
